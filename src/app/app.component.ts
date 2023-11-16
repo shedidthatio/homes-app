7a1db92221fdfd33217d4ad44e593b1aec82fae7
Kat Cue <shedidthatio22@gmail.com>
2023-11-15 20:20:32 -0500

    Created home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hola papa!</h1>`,
+  template: `<main>
+                <header class="brand-name">
+                  <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
